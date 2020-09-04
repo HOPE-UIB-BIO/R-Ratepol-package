a26d2f4f13fb8feea4c20c9a0f24b3a91a23d047
Ondrej Mottl <Ondrej.Mottl@uib.no>
2020-09-04 16:46:04 +0200

    Update:
    1) Description for fc_estimate_RoC
    
    2) Example data

--- R/fc_estimate_RoC.R
+++ R/fc_estimate_RoC.R
@@ -1,106 +1,22 @@
 fc_estimate_RoC <- function (data_source_pollen,
                              data_source_age,
-                             smooth_method = "grim",
+                             smooth_method = "shep",
                              smooth_N_points = 5,
                              smooth_age_range = 500,
                              smooth_N_max = 9,
                              Working_Units = "MW",
                              bin_size = 500,
                              Number_of_shifts = 5,
-                             rand = 1000,
+                             rand = 10000,
                              treads = T,
                              age_uncertainty = T,
                              standardise = T,
                              N_pollen_grains = 150,
-                             DC = "Chord",
+                             DC = "chord",
                              interest_threshold = F,
                              Peak_detection = "GAM",
                              Debug = F)
 {
-  # data_source_pollen = pollen data with species as columns and levels as rows, level ID as row names
-  # data_source_age = list of 2:
-  #                       $ages = dataframe
-  #                                   $sample.id = unique ID of each level
-  #                                   #age = age of level
-  #                       $age_position = matrix with number of columns as number of levels. Each column is one level
-  #                                         each row is one age sequence from bchron
-  #
-  # smoothing of the pollen data
-  # smooth_method = type of smoothing applied for the each of the pollen type
-  #     "none"    = None: Pollen data is not smoothed
-  #     "m.avg"   = Moving average: Each focus value is calculated as average over N(smooth_N_points) number of levels
-  #                   (preferably ½ N before and ½ after focus level, those values are adjusted in the beginning
-  #                   and end of the core. N must be odd number). Note that each calculation is done from scratch
-  #                   and results are saved separately in order to avoid cumulative rounding errors.
-  #     "grim"    = 	Grimm’s smoothing: Similar to moving average but N is not fixed. For each level, N is selected
-  #                   as odd number between N_a (smooth_N_points) and N_b (smooth_N_max), while the maintaining the maximum age
-  #                   difference from the selected levels as smooth_age_range.
-  #     "age.w"   = 	Age weighted average:  Similar to moving average but average is weighted by the age difference
-  #                   from the focus level and multiplied by 1/smooth_age_range. To avoid up-weighting levels,
-  #                   if smooth_age_range/AGEDIFF exceeds 1, it is saved as 1. This means that levels closer than
-  #                   smooth_age_range to the target age are given full weighting, but those farther away are downweighed
-  #                   by an amount increasing with age difference.
-  #     "shep"    =   Shepard's 5-term filter: Smoothing over 5 points following equation:
-  #                   V_NEW=(17*V + 12*(V_((+1) )  + V_((-1) ) )-3*(V_((+2) )  + V_((-2) )))/35 ,
-  #                   where V is focal level value. All values that result smaller than zero are saved as zero
-  # smooth_N_points = Number of points for (need to be an odd number). Used for moving average, Grimm and Age-Weighted
-  # smooth_N_max = maximal number of samples to look in Grimm smoothing
-  # smooth_age_range = maximal age range for both Grimm and Age-weight smoothing
-  #
-  # Working_Units = selection of units that the DC will be calculated between
-  #     "levels"  = DC is calculated between all subssequent levels
-  #     "BINs"    = Selective Binning: BINs (age brackets of various size (bin_size)) are created and  one level is selected as a representation of each BIN.
-  #                 BINS serve as Working Units. BINs without any pollen data are excluded
-  #     "MU"      = Selective Binning with Moving Window: This method follows a simple sequence: BINs are created, levels are selected, and RoC between BINs
-  #                 is calculated, similar to Selective Binning. However, the brackets of BINs (window) are then moved forward by selected amount
-  #                 of time (Z), levels are selected again, and RoC calculated for a new set of WU. This is repeated Number_of_shifts times
-  #                 (where Z = bin_size/Number_of_shifts) while keeping all the results.
-  # bin_size = size of the BIN (in years)
-  # Number_of_shifts = value determining the number of shifts of window
-  #
-  # rand = number of randomization. Age sequence is randomly sampled from age-depth model uncertainties
-  #         at the begining of each run.
-  #
-  # standardise [T/F] = standardise each Working Unit to cetrain number of pollen grains (random resampling without repetition)
-  # N_pollen_grains = Number of grain to perform standardisation to
-  #
-  # DC = disimilarity coeficient. Type of calculation of differences between samples/BINs
-  #   "euc"     = 	Euclidean distance: √(∑_(i=1)^n〖(A_i-B_i)〗^2 ), where A_i and B_i are values for Working Unit A and B given for species i.
-  #   "euc.sd"  = 	Standardised Euclidean distance: √(∑_(i=1)^n〖((A_i-B_i)/〖SD〗_i )〗^2 ) ,
-  #                 where A_i and B_i are values for Working Unit A and B given for species i,and 〖SD〗_i is a standard deviation for species i,
-  #                 calculated from whole sequence.
-  #   "chord"   = 	Chord distance:√(∑_(i=1)^n〖(√(A_i )-√(B_i ))〗^2 ), where A_i and B_i are values for Working Unit A and B given for species i.
-  #   "chisq    = 	Chi-squared coefficient:√(∑_(i=1)^n〖(A_i-B_i)〗^2/((A_i+B_i))), where A_i and B_i are values for Working Unit A and B
-  #                 given for species i
-  #
-  # Comment 1:
-  # Note that DC is calculated between each subsequent Working Units and then standardise by the time difference between them. Age of each RoC values
-  #   is determined as mean between values of Wokring Units.
-  #
-  # Comment 2:
-  # Due to randomisation, there is a chanche than there will be different Working Unit combination  in each run (some Working Units might "fallout" due to
-  #     random time sampling and pollen subsampling). The calculation between two subsequent WU (i.e. one Working Unit combination) results in RoC
-  #     score and time position (calculated as mean time position of WUs). However, due to random selecting of age sequence, each of the Working Unit
-  #     combination will result in multiple RoC values and time positions. R-Ratepol assign the time position of each Working Unit combination as median
-  #     time positions from all randomisations. Final RoC values are calculated as median of scores from all randomisations. In addition, due to
-  #     excluding empty Working Units, there is a chance that some Working Unit combination will be present only in some randomisation.
-  #     Therefore, R-Ratepol only include Working Unit combinations that are present in at least 10% of all randomisations.
-  #
-  # interest_threshold [T/F] = age after which is the data reduced after calucaltion of RoC
-  #
-  # Peak_detection = method of peak detection:
-  #         1) Threshold = Treshold value is set for whole dataset (after subseting for interest_threshold) as median of all RoC values.
-  #                     Peak_detection is  consider significant if 95% quantile (gain from randomisation) is higher than treshold
-  #         2) GAM = Gam model is fitted with RoC and Age. Differences between GAM and each point is calculated. SD is calculated from all the differences
-  #                     Peak_detection is considered significat if it is 1.5 SD higher than GAM
-  #         3) SNI = Signal-to-Noise Index, following adapted the SNI from Kelly et al. (2011)
-  #                     written to detect changes in charcoal records. We SNI I calculated for the whole RoC sequence
-  #                     and point is consider significant if has SNI value higher than 3 (following suggesting
-  #                     from Kelly et al. (2011)).
-  #
-  # Debug [T/F] = show messages from internal processes
-
-
   # CODE
   start.time <- Sys.time()
   if (Debug == T){
@@ -354,8 +270,7 @@ fc_estimate_RoC <- function (data_source_pollen,
   # GAM
   if(Peak_detection == "GAM"){
     # mark points that are abowe the GAM model (exactly 1.5 SD higher than GAM prediction)
-    r.m.full$pred.gam = mgcv::predict.gam(mgcv::gam(RUN.RoC.sm~s(RUN.Age.Pos,k=3), data = r.m.full, family = "Gamma()",
-                                                    method = "REML"), type="response")
+    r.m.full$pred.gam = mgcv::predict.gam(mgcv::gam(RUN.RoC.sm~s(RUN.Age.Pos,k=3), data = r.m.full, family = "Gamma()",method = "REML"), type="response")
     r.m.full$pred.gam.diff = r.m.full$RUN.RoC.sm - r.m.full$pred.gam
     r.m.full$Peak = (r.m.full$pred.gam.diff) > 1.5*sd(r.m.full$pred.gam.diff)
 
