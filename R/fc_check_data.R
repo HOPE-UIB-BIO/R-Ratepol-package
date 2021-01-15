c84c522572fbd717483f967d09ffc316cef8c8ad
Ondřej Mottl <ondrej.mottl@gmail.com>
2021-01-15 11:35:04 +0100

    V 0.5.0: Remove kill.all to separate function - cosmetic changes

--- R/fc_check_data.R
+++ R/fc_check_data.R
@@ -1,34 +1,8 @@
-fc_check_data <- function (data_source_check, proportion = F, Species = T, Samples = T, Debug = F)
+fc_check_data <- function (data_source_check, proportion = F, Species = TRUE, Samples = TRUE, Debug = F)
 {
   # check if there is a sample that do not have a individuals data and delete it
   # & 
   # check if there are any specie without individuals record and delete them
-  
-  kill.all <- function(data_source_check.kill, Species = Species, Samples = Samples){
-    if(any(rowSums(data_source_check.kill$Community, na.rm = T) == 0) & Samples == T){ # if there are some samples without individuals
-      
-      data_source_check.kill$Age <-  
-        data_source_check.kill$Age[rowSums(data_source_check.kill$Community, na.rm = T) > 0, ]
-      
-      data_source_check.kill$Age.un <- 
-        data_source_check.kill$Age.un[ ,rowSums(data_source_check.kill$Community, na.rm = T) > 0]
-      
-      data_source_check.kill$Community <- 
-        data_source_check.kill$Community[rowSums(data_source_check.kill$Community, na.rm = T) > 0, ]
-    }
-    
-    if(any(colSums(data_source_check.kill$Community, na.rm=T)==0) & Species == T){ # if there are some species without individuals
-      
-      data_source_check.kill$Community<- data_source_check.kill$Community[ ,colSums(data_source_check.kill$Community, na.rm = T) > 0]
-    }
-    
-    # count the species and samples
-    data_source_check.kill$Dim.val[1] <-  ncol(data_source_check.kill$Community)
-    data_source_check.kill$Dim.val[2] <-  nrow(data_source_check.kill$Community)
-    data_source_check.kill$Dim.val[3] <-  nrow(data_source_check.kill$Age)
-    
-    return(data_source_check.kill)
-  }
   
   data_source_check <-  
     kill.all(
@@ -36,36 +10,45 @@ fc_check_data <- function (data_source_check, proportion = F, Species = T, Sampl
       Species = Species, 
       Samples = Samples) 
   
-  if(Debug==T){
+  if(Debug == TRUE){
     
-    cat("", fill=T)
-    cat(paste("Community data have", data_source_check$Dim.val[1], "species with records and",
-              data_source_check$Dim.val[2], "samples. Age data have", data_source_check$Dim.val[3], "samples"),
-        fill=T)
+    cat("", fill = TRUE)
+    cat(
+      paste(
+        "Community data have", data_source_check@Dim.val[1], "species with records and",
+        data_source_check@Dim.val[2], "samples. Age data have", data_source_check@Dim.val[3], "samples"),
+      fill = TRUE)
     
-    cat("", fill=T)
-    cat(paste("Age data has values of min", min(data_source_check$Age$age),
-              ", max",max(data_source_check$Age$age),
-              ",mean", round(mean(data_source_check$Age$age),2),
-              ",and median", round(median(data_source_check$Age$age),2)),
-        fill=T)
+    cat("", fill = TRUE)
+    cat(
+      paste(
+        "Age data has values of min", min(data_source_check@Age$age),
+        ", max",max(data_source_check@Age$age),
+        ", mean", round(mean(data_source_check@Age$age), 2),
+        ", and median", round(median(data_source_check@Age$age), 2)),
+      fill = TRUE)
     
-    cat("", fill=T)
+    cat("", fill = TRUE)
   }
   
   # check if all values is new age are in positive values and interpolate if necesery
-  if(any(data_source_check$Age$newage<0, na.rm = T))
+  if(any(data_source_check@Age$newage<0, na.rm = TRUE))
   {
-    data_source_check$Age$newage <- data_source_check$Age$newage + min(data_source_check$Age$newage)*(-1)
+    data_source_check@Age$newage <- 
+      data_source_check@Age$newage + min(data_source_check@Age$newage)*(-1)
   }
   
-  if (proportion == T)
+  if (proportion == TRUE)
   {
-    if (Debug==T){ cat("Community data values are being converted to proportions", fill=T)}
+    if (Debug =TRUE){
+      cat("Community data values are being converted to proportions", fill = TRUE)}
     
     # convert the values community data to proportion of sum of each sample
-    p.counts.row.sums <- apply(data_source_check$Community, 1, sum)
-    data_source_check$Community <- as.data.frame(lapply(data_source_check$Community, function(x) x/p.counts.row.sums))
+    p.counts.row.sums <- apply(data_source_check@Community, 1, sum)
+    
+    data_source_check@Community <- 
+      as.data.frame(lapply(data_source_check@Community, function(x) x/p.counts.row.sums))
+    
     data_source_check <- kill.all(data_source_check, Species = Species, Samples = Samples)
   }
   
