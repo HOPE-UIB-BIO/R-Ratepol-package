96d3434bd27c25677191e729580f71711a7e4a17
Ondřej Mottl <ondrej.mottl@gmail.com>
2021-05-07 16:29:08 +0200

    adding documentation for data files

---
+++ R/gl_bchron.R
@@ -0,0 +1,20 @@
+#' Bchron result used in the vignette Example of full workflow"
+
+#' @format A list of 12:
+#' \describe{
+#'  \item{theta}{}
+#'  \item{phi}{} 
+#'  \item{mu}{}
+#'  \item{thetaPredict}{}
+#'  \item{predictPositions}{}
+#'  \item{positions}{}
+#'  \item{extractDate}{}
+#'  \item{ageScaleVal}{}
+#'  \item{positionNormalise}{}
+#'  \item{inputVals}{}
+#' }
+#' @examples
+#' \dontrun{
+#'  gl_bchron
+#' }
+"gl_bchron"
