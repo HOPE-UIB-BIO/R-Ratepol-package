b399f48dbb6da2110b7498f0e59a9dcd800b9d4d
Ondřej Mottl <ondrej.mottl@gmail.com>
2021-05-07 15:11:27 +0200

    Restructure to allow the data to have 'sample_id' or 'sample.id' in as column name.

--- R/fc_extract_data.R
+++ R/fc_extract_data.R
@@ -8,39 +8,65 @@ fc_extract_data <-  function (data_community_extract,
     cat(paste("Data extraction started", Sys.time()), fill = TRUE)
   }
   
-  # Initial tests ----
+  # 1. Initial tests -----
+
+  n_samples_com <- nrow(data_community_extract)
+  n_samples_age <- nrow(data_age_extract)
   
-  # community data
+  # 1.1 Data types -----
+  
   assertthat::assert_that(
     'data.frame' %in% class(data_community_extract),
     msg = "Object `data_source_community` must be a `data.frame`")
   
-  n_samples_com <- nrow(data_community_extract)
-  n_species_com <- ncol(data_community_extract)
-  
-  assertthat::assert_that(
-    'sample.id' %in% names(data_community_extract),
-    msg = "Variable `sample.id` must be present in `data_source_community`")
-  
-  assertthat::assert_that(
-    'character' %in% class(data_community_extract$sample.id),
-    msg = "Variable `sample.id` in `data_source_community` must be a `character`")
-  
-  
-  # age data
   assertthat::assert_that(
     'data.frame' %in% class(data_age_extract),
     msg = "Object `data_source_age` must be a `data.frame`")
   
-  n_samples_age <- nrow(data_age_extract)
+  # 1.2. Sample id -----
   
   assertthat::assert_that(
-    'sample.id' %in% names(data_age_extract),
-    msg = "Variable `sample.id` must be present in `data_source_age`")
+    'sample.id' %in% names(data_community_extract) | 
+      'sample_id' %in% names(data_community_extract)  ,
+    msg = "Variable `sample.id` or `sample_id` must be present in the 
+    `data_source_community`")
+  
+  assertthat::assert_that(
+    'sample.id' %in% names(data_age_extract) |
+      'sample_id' %in% names(data_age_extract) ,
+    msg = "Variable `sample.id` or `sample_id` must be present in the 
+    `data_source_age`")
+
+  # community
+  if('sample_id' %in% names(data_community_extract)){
+    data_community_extract <- 
+      data_community_extract %>% 
+      dplyr::rename(sample.id  = sample_id)
+  }
+  
+  assertthat::assert_that(
+    'character' %in% class(data_community_extract$sample.id),
+    msg = "Variable `sample.id` or `sample_id` in `data_source_community` must
+    be a `character`")
+  
+  #age
+  if('sample_id' %in% names(data_age_extract)){
+    data_age_extract <- 
+      data_age_extract %>% 
+      dplyr::rename(sample.id  = sample_id)
+  }
   
   assertthat::assert_that(
     'character' %in% class(data_age_extract$sample.id),
-    msg = "Variable `sample.id` in `data_source_age` must be a `character`")
+    msg = "Variable `sample.id` in or `sample_id` `data_source_age` must be
+    a `character`")
+  
+  assertthat::assert_that(
+    all(data_community_extract$sample.id == data_age_extract$sample.id),
+    msg = "Variable `sample.id`/`sample_id` must have same values in 
+    `data_source_age` and `data_source_community`")
+  
+  # 1.3. Age test -----
   
   assertthat::assert_that(
     'age' %in% names(data_age_extract),
@@ -72,16 +98,17 @@ fc_extract_data <-  function (data_community_extract,
     
   }
   
-  # both
+  # 1.4. Size test ----- 
+  
   assertthat::assert_that(
     n_samples_com == n_samples_age,
     msg = "Object `data_source_community` and `data_source_age` 
     must have the same number of levels")
+
+ 
+  # 2. Processes data ----- 
   
-  assertthat::assert_that(
-    all(data_community_extract$sample.id == data_age_extract$sample.id),
-    msg = "Variable `sample.id` must have same values in 
-    `data_source_age` and `data_source_community`")
+  # 2.1 Extract data  ----- 
   
   # extract both important tables if stored as tibbles
   age <- as.data.frame(data_age_extract)
@@ -90,8 +117,10 @@ fc_extract_data <-  function (data_community_extract,
   # exclude any other varibales than age and sample id
   age <-  age[ ,names(age) %in% c("sample.id", "age")]
   
+  # 2.2 Age uncertainty  ----- 
+  
   # if age_uncertainty is used
-  if (!all(age_uncertainty == F)){
+  if (!all(age_uncertainty == FALSE)){
     assertthat::assert_that(
       'matrix' %in% class(age_uncertainty),
       msg = "Object `age_uncertainty` must be a `matrix`")
@@ -99,7 +128,7 @@ fc_extract_data <-  function (data_community_extract,
     n_samples_un <- ncol(age_uncertainty)
     
     assertthat::assert_that(
-      n_samples_un == n_samples_un,
+      n_samples_age == n_samples_un,
       msg = "Object `data_source_age` and `age_uncertainty` must have 
       the same number of levels. `age_uncertainty` must have samples stored as 
       columns")
@@ -111,8 +140,8 @@ fc_extract_data <-  function (data_community_extract,
     age.un <- data.frame(t(matrix(rep(age$age, 10), ncol = 10)))
   }
   
-  # sample.id ----
-
+  # 2.3 Row.names  ----- 
+  
   # add row.names to commity, age, and uncertainty data
   row.names(dat_community) <-  dat_community$sample.id
   row.names(age) <-  age$sample.id
@@ -125,20 +154,22 @@ fc_extract_data <-  function (data_community_extract,
   # Newage is a value of interpolated time (time which start with 0)
   age$newage <-age$age
   
-  # dim.val are values of the size of the dataset
-  dim.val <- vector(mode = "integer", length = 3)
-  names(dim.val) <- c("N Species","N samples community","N samples Age")
+  # 2.4 Summary  -----   
   
-  # create list  class of 4 variables Ppllen, age, age.un, dim.val
-  dat.merge <- 
+  # dim_val are values of the size of the dataset
+  dim_val <- vector(mode = "integer", length = 3)
+  names(dim_val) <- c("N Species","N samples community","N samples Age")
+  
+  # create list  class of 4 variables Ppllen, age, age.un, dim_val
+  dat_merge <- 
     RRatepol:::RRatepolList(
       Community = dat_community,
       Age = age,
       Age.un = age.un,
-      Dim.val = dim.val)
+      Dim.val = dim_val)
   
   # perform check = round number of species and samples and exclude "empty" ones
-  dat.merge <- fc_check_data(dat.merge, proportion = F, Debug = Debug)
+  dat_merge <- fc_check_data(dat_merge, proportion = F, Debug = Debug)
   
   if (Debug==T)
   {
@@ -147,5 +178,5 @@ fc_extract_data <-  function (data_community_extract,
     cat("",fill = T)
   }
   
-  return(dat.merge)
+  return(dat_merge)
 }
