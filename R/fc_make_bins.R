4df5231e582ba265299d9cc6adef8d54edd283db
Ondrej Mottl <ondrej.mottl@gmail.com>
2022-07-18 10:45:15 +0200

    data preparation simplification

--- R/fc_make_bins.R
+++ R/fc_make_bins.R
@@ -1,30 +1,101 @@
 fc_make_bins <-
-  function(data_source_bin,
-           bin_size = 500) {
-    util_check_class("data_source_bin", "list")
+    function(data_source_bins,
+             Working_Units = c("levels", "bins", "MW"),
+             bin_size = 500,
+             Number_of_shifts = 5) {
+        util_check_class("data_source_bins", "list")
 
-    util_check_class("bin_size", "numeric")
+        util_check_class("Working_Units", "character")
 
-    util_check_if_integer("bin_size")
+        util_check_vector_values(
+            "Working_Units",
+            c("levels", "bins", "MW")
+        )
 
-    bin_oldest <-
-      ceiling(
-        max(data_source_bin$age$age)
-      )
+        Working_Units <- match.arg(Working_Units)
 
-    bin_youngest <-
-      floor(
-        min(data_source_bin$age$age)
-      )
+        if (
+            Working_Units == "levels"
+        ) {
+            n_levels <-
+                nrow(data_source_prep$age)
 
-    bin_breaks <- seq(
-      from = bin_youngest,
-      to = bin_oldest,
-      by = bin_size
-    )
+            n_res <- n_levels - 1
 
-    data.frame(
-      name = bin_breaks
-    ) %>%
-      return()
-  }
+            res <-
+                data.frame(
+                    name = rownames(data_source_prep$age)[1:n_res]
+                ) %>%
+                dplyr::mutate(
+                    start = as.character(name),
+                    end = as.character(
+                        rownames(data_source_prep$age)[2:n_levels]
+                    ),
+                    age = data_source_prep$age$age[1:n_res],
+                    label = paste(start, end, sep = "-")
+                )
+            return(res)
+        }
+
+        util_check_class("bin_size", "numeric")
+
+        util_check_if_integer("bin_size")
+
+        bin_oldest <-
+            ceiling(
+                max(data_source_bins$age$age)
+            )
+
+        bin_youngest <-
+            floor(
+                min(data_source_bins$age$age)
+            )
+
+        bin_breaks <- seq(
+            from = bin_youngest,
+            to = bin_oldest,
+            by = bin_size
+        )
+
+        if (
+            Working_Units == "bins"
+        ) {
+            res_df <-
+                data.frame(
+                    name = bin_breaks
+                )
+        } else {
+            util_check_class("Number_of_shifts", "numeric")
+
+            util_check_if_integer("Number_of_shifts")
+        }
+
+        if (
+            Working_Units == "MW"
+        ) {
+            shift_value <- bin_size / Number_of_shifts
+
+            shift_increment <-
+                shift_value * sort(
+                    rep(0:(Number_of_shifts - 1), length(bin_breaks))
+                )
+
+            res_df <-
+                data.frame(
+                    name = (rep(bin_breaks, Number_of_shifts) +
+                        shift_increment),
+                    shift = sort(rep(c(1:Number_of_shifts), length(bin_breaks)))
+                )
+        }
+
+        res <-
+            res_df %>%
+            dplyr::mutate(
+                start = name,
+                end = start + bin_size,
+                age = (end + start) / 2,
+                label = paste(start, end, sep = "-")
+            )
+
+        return(res)
+    }
--- R/fc_prepare_data.R
+++ R/fc_prepare_data.R
@@ -12,55 +12,120 @@ fc_prepare_data <-
 
         Working_Units <- match.arg(Working_Units)
 
-        if (
-            Working_Units != "levels"
-        ) {
-            util_check_class("bin_size", "numeric")
+        util_check_class("bin_size", "numeric")
 
-            util_check_if_integer("bin_size")
-
-            if (
-                Working_Units == "MW"
-            ) {
-                util_check_class("Number_of_shifts", "numeric")
-
-                util_check_if_integer("Number_of_shifts")
-            }
-        }
+        util_check_if_integer("bin_size")
 
         util_check_class("rand", c("NULL", "numeric"))
 
+        # check the condition
+        is_rand_present <-
+            (is.null(rand) == FALSE)
+
         if (
-            is.null(rand) == FALSE
+            is_rand_present == TRUE
         ) {
             util_check_if_integer("rand")
         }
 
-        res <-
-            switch(Working_Units,
-                "levels" = {
-                    fc_prepare_data_levels(
-                        data_source_prep = data_source_prep,
-                        rand = rand
-                    )
-                },
-                "bins" = {
-                    fc_prepare_data_bins(
-                        data_source_prep = data_source_prep,
-                        bin_size = bin_size,
-                        Number_of_shifts = 0,
-                        rand = rand
-                    )
-                },
-                "MW" = {
-                    fc_prepare_data_bins(
-                        data_source_prep = data_source_prep,
-                        bin_size = bin_size,
-                        Number_of_shifts = Number_of_shifts,
-                        rand = rand
-                    )
-                }
-            )
+        is_shift_present <-
+            Working_Units == "MW" && (Number_of_shifts != 0)
 
-        return(res)
+        if (
+            Working_Units == "levels"
+        ) {
+            bin_dummy <-
+                fc_make_bins(
+                    data_source_bin = data_source_prep,
+                    Working_Units = "levels"
+                )
+        } else if (
+            is_shift_present == TRUE
+        ) {
+            bin_dummy <-
+                fc_make_bins(
+                    data_source_bin = data_source_prep,
+                    Working_Units = "MW",
+                    bin_size = bin_size,
+                    Number_of_shifts = Number_of_shifts
+                )
+        } else {
+            bin_dummy <-
+                fc_make_bins(
+                    data_source_bin = data_source_prep,
+                    Working_Units = "bins",
+                    bin_size = bin_size
+                )
+        }
+
+        is_uncertit_present <-
+            (is.null(data_source_prep$age_un) == FALSE)
+
+        if (
+            is_uncertit_present == TRUE && is_rand_present == TRUE
+        ) {
+            random_value <-
+                sample(
+                    c(
+                        1:max(1, nrow(data_source_prep$age_un))
+                    ),
+                    rand
+                )
+        }
+
+        # test individual possibilities
+        if (
+            is_rand_present == FALSE
+        ) {
+            list(
+                list(
+                    data = data_source_prep[c("community", "age")],
+                    bins = bin_dummy
+                )
+            ) %>%
+                rlang::set_names(
+                    nm = 1
+                ) %>%
+                return()
+        } else if (
+            # is_rand_present is TRUE
+            is_uncertit_present == FALSE
+        ) {
+            rep(
+                list(
+                    list(
+                        data = data_source_prep[c("community", "age")],
+                        bins = bin_dummy
+                    )
+                ),
+                rand
+            ) %>%
+                rlang::set_names(
+                    nm = 1:rand
+                ) %>%
+                return()
+        } else {
+            # is_uncertit_present is TRUE
+            c(1:rand) %>%
+                rlang::set_names(
+                    nm = 1:rand
+                ) %>%
+                purrr::map(
+                    .f = ~ {
+                        data_temp <-
+                            data_source_prep
+
+                        data_temp$age$age <-
+                            as.numeric(data_temp$age_un[random_value[.x], ])
+
+                        list(
+                            data = data_temp[c("community", "age")],
+                            bins = bin_dummy
+                        ) %>%
+                            return()
+                    }
+                ) %>%
+                return()
+        }
+       # stop("Not a valid setting for creating bins")
     }
