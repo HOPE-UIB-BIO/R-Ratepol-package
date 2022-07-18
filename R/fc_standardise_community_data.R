8e6ce953ae7aa7e951e9b778ac493570d07f5a3f
Ondrej Mottl <ondrej.mottl@gmail.com>
2022-07-18 16:15:00 +0200

    refactor of whole function

--- R/fc_standardise_community_data.R
+++ R/fc_standardise_community_data.R
@@ -1,40 +1,58 @@
-fc_standardise_community_data <- function (data_source, N_individuals = 150, verbose = FALSE){
-  
-  if(verbose == TRUE){
-    cat(paste("Data standardization started", Sys.time()), fill = TRUE)}
-  
-  Samples <-  1:nrow(data_source@Community) 
-  Samples <-  Samples[!is.na(data_source@Age$sample.id)]
-  
-  for(i in Samples){  # for each row(sample)
-    
-    select_row <-  data_source@Community[i, ] # selected row
-    
-    n1 <-  1:ncol(data_source@Community) #number for each species name in community data
-    ab1 <-  as.vector(select_row) #frequencies of species in each sample
-    
-    vec1 <-  NULL    #a vector for the species pool
-    
-    # create a vector with species numbers replicated X times, where X is number of individuals
-    for(j in 1:length(n1))  {
-      
-      #1: number of species 
-      v1 <-  rep(n1[j], ab1[j]) #repeat species names 
-      vec1 <-  c(vec1, v1) #a vector that repeat the species for the occurrences
+fc_standardise_community_data <-
+  function(data_source_standard,
+           N_individuals = 150) {
+    data_community <-
+      util_subset_community(data_subset)  %>% 
+      round()
+
+    n_taxa <-
+      ncol(data_community)
+
+    # for each row(sample)
+    for (i in 1:nrow(data_community)) {
+
+      # selected row
+      select_row <-
+        data_community[i, ]
+
+      # a vector for the species pool
+      vec1 <- vector(, length = 0)
+
+      # create a vector with species numbers replicated X times,
+      #  where X is number of individuals
+      for (j in 1:n_taxa) {
+
+        # repeat species names
+        v1 <-
+          rep(
+            names(select_row)[j],
+            select_row[j]
+          )
+        vec1 <-
+          c(vec1, v1) # a vector that repeat the species for the occurrences
       }
-    
-    # sample species X time (150 times)
-    rsample <-  sample(vec1, size = N_individuals, replace = FALSE)
-    
-    # replace all values in community data by 0
-    data_source@Community[i, ]<-  rep(0,length(select_row))
-    
-    # replace individuals by new randomised values
-    data_source@Community[i,as.numeric(names(table(rsample)))] <- as.numeric(table(rsample))
-    
+
+      # sample species X time
+      rsample <-
+        sample(vec1,
+          size = N_individuals,
+          replace = FALSE
+        )
+
+      sel_names <-
+        table(rsample)
+
+      # replace all values in community data by 0
+      data_community[i, ] <-
+        rep(0, n_taxa)
+
+      # replace individuals by new randomised values
+      data_community[i, names(sel_names)] <-
+        as.numeric(sel_names)
+    }
+
+    data_source_standard[, names(data_community)] <-
+      data_community
+
+    return(data_source_standard)
   }
-  
-  if(verbose == TRUE){cat(paste("Data standardization finished", Sys.time()), fill = TRUE)}
-  
-  return (data_source) 
-}
