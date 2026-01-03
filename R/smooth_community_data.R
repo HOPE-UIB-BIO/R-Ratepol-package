30bad5de6d9917b4006a7c918c4539bb00adb29e
Friederike Johanna Rosa Wölke <wolke@fzp.czu.cz>
2026-01-03 20:23:42 +0100

    Make `util_search_parameter()` separate file (#108)

--- R/smooth_community_data.R
+++ R/smooth_community_data.R
@@ -174,46 +174,6 @@ smooth_community_data <-
       }
     }
 
-    # crete helper function for GRIMM smoothing
-    # test if this increase does not invalidate rules:
-    #   1) seach parameter cannot go outside of the sample size
-    #     (up or down)
-    #   2) seach parameter cannot be biger than selected maximum sample
-    #     sizes
-    #   3) the age difference between samples selected by the seach
-    #     paramated cannot be higher than defined max age range if all
-    #     of those ARE TRUE then increase the real search parameter
-
-    util_search_parameter <-
-      function(A, B, smooth_age_range) {
-        for (k in 1:(smooth_n_max - smooth_n_points)) {
-          # create new search parameter that is lower by 1
-          A_test <- A - 1
-          if (
-            A_test > 0 && B - A_test < smooth_n_max
-          ) { # i+N.active.test < nrow(dat_community) &
-            if (
-              abs(dat_age$age[A_test] - dat_age$age[B]) < smooth_age_range
-            ) {
-              A <- A_test
-            }
-          }
-
-          # create new search parameter that higher by 1
-          B_test <- B + 1
-          if (
-            B_test < nrow(dat_community) && B - A_test < smooth_n_max
-          ) {
-            if (
-              abs(dat_age$age[A] - dat_age$age[B_test]) < smooth_age_range
-            ) {
-              B <- B_test
-            }
-          }
-        }
-        return(c(A, B))
-      }
-
     # ----------------------------------------------
     # Additional information -----
     # ----------------------------------------------
@@ -332,7 +292,11 @@ smooth_community_data <-
               util_search_parameter(
                 focus_par[i, 1],
                 focus_par[i, 2],
-                smooth_age_range
+                smooth_age_range,
+                smooth_n_max,
+                smooth_n_points,
+                dat_age,
+                dat_community
               )
           } else {
             # Samples near end
@@ -349,7 +313,11 @@ smooth_community_data <-
                 util_search_parameter(
                   focus_par[i, 1],
                   focus_par[i, 2],
-                  smooth_age_range
+                  smooth_age_range,
+                  smooth_n_max,
+                  smooth_n_points,
+                  dat_age,
+                  dat_community
                 )
             } else {
               focus_par[i, 1] <-
@@ -362,7 +330,11 @@ smooth_community_data <-
                 util_search_parameter(
                   focus_par[i, 1],
                   focus_par[i, 2],
-                  smooth_age_range
+                  smooth_age_range,
+                  smooth_n_max,
+                  smooth_n_points,
+                  dat_age,
+                  dat_community
                 )
             }
           }
